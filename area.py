ee693fb83f2d7a3fa9b8c7e147349532f21f85ec
bfc-raghav <30250525@blackpool.ac.uk>
2024-11-06 10:02:11 +0000

    Update area.py

--- area.py
+++ area.py
@@ -2,14 +2,19 @@
 # Python program to calculate the area of a rectangle
 
 def calculate_area(length, width):
-    area = length + width
+    area = length * width
     return area
 
-length = input("Enter the length of the rectangle: ")
-width = input("Enter the width of the rectangle: ")
-
-length = int(length)
-width = int(width)
+try:
+    length = int(input("Enter the length of the rectangle: "))
+except:
+    print("The value given is not a integer. A default value of 7 will be used")
+    length = 7
+try:
+    width = int(input("Enter the width of the rectangle: "))
+except: 
+    print("The value given is not an integer. A default value of 12 will be used.")
+    width = 12
 
 print("The area of the rectangle is:", calculate_area(length, width))
 
